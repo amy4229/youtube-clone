a9e3a13f8cb7b37ad30c3349e1c18ab079ea9351
amy <amykim4229@gmail.com>
2022-06-14 00:55:46 +0900

    220613 Add searchBar
    make function for searching videos
    without keyword, it shows the recomment popular videos

---
+++ src/components/searchHeader/searchHeader.jsx
@@ -0,0 +1,37 @@
+import React, { createRef } from "react";
+import styles from "./searchHeader.module.css";
+
+const searchHeader = (props) => {
+    const ref = createRef();
+
+    const onSearchClick =(event) =>{
+        event.preventDefault();
+        const keyword = ref.current.value;
+        props.setKeyword(keyword);
+    }
+   const onLogoClick = ()=> {
+    props.setKeyword('');
+   }
+
+  return (
+    <header>
+      <img
+        className={styles.logo}
+        src="/images/YouTube_Logo.svg.png"
+        alt="youtube logo"
+        onClick={onLogoClick}
+      />
+      <form className={styles.form} onSubmit={onSearchClick}>
+        <input
+          className={styles.searchInput}
+          ref={ref}
+        />
+        <button className={styles.button}>
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </form>
+    </header>
+  );
+};
+
+export default searchHeader;
