9307824efabd6a66a0c81be200b6e0594a3d41a0
amy <amykim4229@gmail.com>
2022-06-17 15:23:02 +0900

    0616 ADD StringUtil
    # remove HTML escape string
    # make formatting date str

--- src/util/stringUtil.js
+++ src/util/stringUtil.js
@@ -2,3 +2,7 @@ import ReactHtmlParser from 'react-html-parser';
 export const unescapeString = (str) => {
     return ReactHtmlParser(str);
 }
+
+export const makeFomattedDateStr = (date) => {
+   return date.replaceAll(/[Z,T]/gi," ");
+}
