d240dd969463d1227ee6f26fc87eafa208e177d2
amy <amykim4229@gmail.com>
2022-06-17 15:26:35 +0900

    220616 Change app structure

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,17 @@
-import { StrictMode } from 'react';
+import { StrictMode,useState} from 'react';
 import "./app.css";
-import Main from "./components/main/main.jsx";
+import SearchHeader from "./components/searchHeader/searchHeader.jsx";
+import VideoMain from './components/video_Main/video_main';
+import ApiUtil from './util/apiUtil';
 
 function App() {
+  const apiUtil = new ApiUtil();
+  const [keyword, setKeyword] = useState('');
+
   return (
     <StrictMode>
-      <Main />
+      <SearchHeader setKeyword={setKeyword}/>
+      <VideoMain  api = {apiUtil} keyword = {keyword}/>
     </StrictMode>
   );
 }
